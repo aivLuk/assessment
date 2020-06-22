993e4f6bfd70f5323652f3632c5b74c6a2af13be
Aivaras <lukauskasaivaras@gmail.com>
2020-06-22 11:23:01 +0300

    tech task

--- SV/sv.js
+++ SV/sv.js
@@ -17,32 +17,3 @@ const validSolution = (arr) => {
         }
     });
 }
-
-//the sum that I calculated is 4613732
-const fibonacciEvenSum = (number) => {
-    let fibArr = [];
-
-    let a, b, c;
-    a = 1;
-    b = 2;
-
-    fibArr.push(a);
-    fibArr.push(b);
-
-    while ((a + b) < number) {
-        c = a + b;
-        a = b;
-        b = c;
-        fibArr.push(c);
-    }
-
-    let sum = 0;
-    fibArr.forEach(el => {
-        if (el % 2 === 0) {
-            sum = sum + el;
-        }
-    })
-    console.log(sum);
-}
-
-fibonacciEvenSum(4000000)
